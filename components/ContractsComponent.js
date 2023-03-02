57031895370043a36607404a31cc8c8d4c32e1b9
Tucker <borisblade17@gmail.com>
2023-03-01 21:20:46 -0600

    Add componet to display contracts - Tucker

--- components/ContractsComponent.js
+++ components/ContractsComponent.js
@@ -1,7 +1,69 @@
-import { StyleSheet, View, Text } from 'react-native'; import Constants from 'expo-constants';
-import { useTheme } from 'react-native-paper';
+import { StyleSheet, View, Text, ScrollView } from 'react-native'; import Constants from 'expo-constants';
+import { Button, Card, Modal, TextInput, useTheme } from 'react-native-paper';
+import { useState, useEffect } from 'react';
+import { PaperSelect } from 'react-native-paper-select';
+import { runTransaction } from '../util';
+import UseYearComponent from './UseYearComponent';
 
-export function ContractsComponent() {
+const contracts = [{
+  contractId: 3,
+  homeResort: 'Animal Kingdom',
+  points: 100,
+  useYear: 'February'
+}]
+
+const contractsSameUseYear = [{
+  contractId: 1,
+  homeResort: 'Riviera',
+  points: 200,
+  useYear: 'February'
+}, {
+  contractId: 2,
+  homeResort: 'Old Key West',
+  points: 150,
+  useYear: 'February'
+}]
+
+export function ContractsComponent({db}) {
+  const [contractPoints, setContractPoints] = useState(undefined);
+  const [openAddContract, setOpenAddContract] = useState(false);
+  const [homeResort, setHomeResort] = useState({
+    value: '',
+    list: [],
+    selectedList: [],
+    error: '',
+  });
+  const [useYear, setUseYear] = useState({
+    value: '',
+    list: [
+      { _id: 'feb', value: 'February' },
+      { _id: 'mar', value: 'March' },
+      { _id: 'apr', value: 'April' },
+      { _id: 'jun', value: 'June' },
+      { _id: 'aug', value: 'August' },
+      { _id: 'sep', value: 'September' },
+      { _id: 'oct', value: 'October' },
+      { _id: 'dec', value: 'December' },
+    ],
+    selectedList: [],
+    error: '',
+  });
+
+  const fetchResorts = async () => {
+    if (db !== undefined) {
+      const foundResorts = await runTransaction(db, 'select * from resort order by resort_id ASC;');
+      const builtResorts = foundResorts.map(resort => {
+        return { _id: resort.resort_id, value: resort.name }
+      });
+      setHomeResort({...homeResort, list: builtResorts})
+    }
+  }
+
+  useEffect(() => {
+    if (db !== undefined) {
+      fetchResorts();
+    }
+  }, [db])
 
   const theme = useTheme();
 
@@ -9,21 +71,134 @@ export function ContractsComponent() {
     container: {
       flex: 1,
       backgroundColor: theme.colors.primaryContainer,
-      padding: 10,
-      alignItems: 'center'
+      alignItems: 'center',
+      display: 'flex',
     },
     statusBar: {
       height: Constants.statusBarHeight,
       backgroundColor: theme.colors.primaryContainer,
     },
+    buttonContainer: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      marginBottom: '10%',
+    },
+    addButtonText: {
+      fontSize: 20,
+      paddingLeft: 10,
+      paddingRight: 10
+    },
+    pageTitle: {
+      marginVertical: 20,
+      textAlign: 'center'
+    },
+    addContractContainer: {
+      margin: '5%',
+      backgroundColor: theme.colors.tertiary,
+      margin: 20,
+      height: 500,
+      width: '90%',
+      borderRadius: 5,
+    },
+    modalTitleRow: {
+      display: 'flex',
+      flexDirection: 'row',
+      borderBottomWidth: 3,
+      borderBottomColor: theme.colors.primary,
+      alignItems: 'center'
+    },
+    modalTitle: {
+      flex: 1,
+      fontSize: 24,
+      borderBottomColor: theme.colors.primary,
+      margin: 10
+    },
+    saveButton: {
+      flex: 1,
+      justifyContent: 'flex-end',
+    },
+    saveButtonText: {
+      fontSize: 18,
+      paddingLeft: 10,
+      paddingRight: 10
+    },
+    contractInputSection: {
+      flex: 1,
+      display: 'flex'
+    },
+    scrollContainer: {
+      width: '90%',
+      height: Constants.height
+    }
   });
+
+  const onDismissAddContracts = () => {
+    setContractPoints(undefined)
+    setUseYear({...useYear, value: ''})
+    setHomeResort({...homeResort, value: ''})
+    setOpenAddContract(false)
+  }
+
+  const handlePointsChange = (value) => {
+    setContractPoints(value)
+  }
 
   return (
     <>
       <View style={styles.statusBar}></View>
       <View style={styles.container}>
-        <View style={styles.statusBar}></View>
-        <Text>Enter your contracts here!</Text>
+        <View style={styles.pageTitle}>
+          <Text style={{ fontSize: 45, fontWeight: 'bold', textAlign: 'center', color: '#00232c' }}>Contracts</Text>
+        </View>
+        <ScrollView style={styles.scrollContainer}>
+          <UseYearComponent contractsForUseYear={contractsSameUseYear} useYear={'February'}/>
+          <UseYearComponent contractsForUseYear={contracts} useYear={'February'}/>
+        <View style={styles.buttonContainer}>
+          <Button style={styles.addButton} labelStyle={styles.addButtonText} onPress={() => setOpenAddContract(true)} mode="contained">Add New Contract</Button>
+        </View>
+        </ScrollView>
+        <Modal visible={openAddContract} onDismiss={onDismissAddContracts} contentContainerStyle={styles.addContractContainer}>
+          <View style={styles.modalTitleRow}>
+            <Text style={styles.modalTitle}>Add New Contract</Text>
+          </View>
+          <View style={styles.contractInputSection}>
+          <PaperSelect
+              label="Home Resort"
+              value={homeResort.value}
+              onSelection={(value) => {
+                setHomeResort({
+                  ...homeResort,
+                  value: value.text,
+                  selectedList: value.selectedList,
+                  error: '',
+                });
+              }}
+              arrayList={[...homeResort.list]}
+              selectedArrayList={[...homeResort.selectedList]}
+              errorText={homeResort.error}
+              multiEnable={false}
+            />
+            <TextInput label='Points' value={contractPoints} onChange={value => handlePointsChange(value)}></TextInput>
+            <PaperSelect
+              label="Use Year"
+              value={useYear.value}
+              onSelection={(value) => {
+                setUseYear({
+                  ...useYear,
+                  value: value.text,
+                  selectedList: value.selectedList,
+                  error: '',
+                });
+              }}
+              arrayList={[...useYear.list]}
+              selectedArrayList={[...useYear.selectedList]}
+              errorText={useYear.error}
+              multiEnable={false}
+            />
+            <Button onPress={onDismissAddContracts} labelStyle={styles.addButtonText} style={styles.saveButton}>Save</Button>
+          </View>
+
+        </Modal>
       </View>
     </>
   )
---
+++ components/UseYearComponent.js
@@ -0,0 +1,162 @@
+import Constants from 'expo-constants';
+import { StyleSheet, View } from "react-native";
+import { Card, Text, useTheme } from "react-native-paper";
+
+
+const yearlyPoints = [{
+  yearlyPointsId: 1,
+  contractId: 1,
+  year: 2023,
+  amountAvailable: 154,
+  amountBanked: 0,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 2,
+  contractId: 1,
+  year: 2024,
+  amountAvailable: 200,
+  amountBanked: 0,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 3,
+  contractId: 1,
+  year: 2025,
+  amountAvailable: 200,
+  amountBanked: 0,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 4,
+  contractId: 2,
+  year: 2023,
+  amountAvailable: 0,
+  amountBanked: 0,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 5,
+  contractId: 2,
+  year: 2024,
+  amountAvailable: 150,
+  amountBanked: 0,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 6,
+  contractId: 2,
+  year: 2025,
+  amountAvailable: 150,
+  amountBanked: 0,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 7,
+  contractId: 3,
+  year: 2023,
+  amountAvailable: 0,
+  amountBanked: 50,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 8,
+  contractId: 3,
+  year: 2024,
+  amountAvailable: 150,
+  amountBanked: 0,
+  amountBorrowed: 0
+}, {
+  yearlyPointsId: 9,
+  contractId: 3,
+  year: 2025,
+  amountAvailable: 100,
+  amountBanked: 0,
+  amountBorrowed: 0
+}]
+
+export default function UseYearComponent({ contractsForUseYear, useYear }) {
+
+  const theme = useTheme();
+
+  const styles = StyleSheet.create({
+    useYearContainer: {
+      padding: 10,
+      marginVertical: 10,
+      marginHorizontal: 5,
+    },
+    useYearLabel: {
+      fontSize: 18,
+      color: theme.colors.primary,
+    },
+    resortRow: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 10,
+    },
+    homeResortLabel: {
+      fontSize: 24,
+      flex: 1,
+      fontWeight: 'bold',
+      color: theme.colors.primary,
+    },
+    contractPoints: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: theme.colors.primary,
+    },
+    yearRow: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 5,
+    },
+    yearLabel: {
+      flex: 1,
+      fontSize: 16,
+      color: theme.colors.primary,
+    },
+    availablePoints: {
+      fontSize: 16,
+      color: theme.colors.primary,
+    },
+  });
+
+  const buildAvailablePointsBlock = (contract) => {
+    const { contractId, useYear, points: fullPoints } = contract
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth()
+    const currentYear = currentDate.getFullYear()
+    let yearToDisplay = currentYear
+    // convert useYear to - based index
+    // for feb its 1 
+    const useYearIndex = 1;
+    // fetch all the yearly point rows for the contract - display the current and the next two (3 total)
+    if (useYearIndex < currentMonth) {
+      yearToDisplay = currentYear - 1;
+    }
+    const lastYearToDisplay = yearToDisplay + 2;
+    const availablePoints = [];
+    while (yearToDisplay <= lastYearToDisplay) {
+      // fetch yearly point for contract and year
+      availablePoints.push(yearlyPoints.filter(yearlyPoint => yearlyPoint.contractId === contractId && yearlyPoint.year === yearToDisplay)[0]);
+      yearToDisplay++;
+    }
+    return (
+      availablePoints?.map(availablePoint => (
+        <View style={styles.yearRow}><Text style={styles.yearLabel}>{availablePoint.year}</Text><Text style={styles.availablePoints}>{`${availablePoint.amountAvailable} pts`}</Text></View>
+      )))
+  }
+
+  return (
+    <>
+
+      <Card style={styles.useYearContainer} >
+        <Text style={styles.useYearLabel}>{useYear}</Text>
+        {contractsForUseYear?.map((contract) => (
+          <>
+
+            <View style={styles.resortRow}><Text style={styles.homeResortLabel}>{contract.homeResort}</Text><Text style={styles.contractPoints}>{`${contract.points} pts`} </Text></View>
+            <View style={styles.availablePoints}>
+              {buildAvailablePointsBlock(contract)}
+            </View>
+          </>
+        ))}
+      </Card>
+    </>
+  )
+}
